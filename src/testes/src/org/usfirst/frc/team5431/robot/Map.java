abeeb048b47c9fdb8ceaf8fc86eff5fb3caad24c
Benjamin Emanuel Sher <benjaminesher@gmail.com>
2016-01-14 17:08:44 -0600

    First Commit

---
+++ src/testes/src/org/usfirst/frc/team5431/robot/Map.java
@@ -0,0 +1,8 @@
+package org.usfirst.frc.team5431.robot;
+
+public class Map {
+	public static final int FrontLeftMotor = 4;
+	public static final int RearLeftMotor = 2;
+	public static final int FrontRightMotor = 5;
+	public static final int RearRightMotor = 3;
+}
